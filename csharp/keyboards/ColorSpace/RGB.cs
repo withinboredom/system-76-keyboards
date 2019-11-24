6e606ed99eba64c7575df0fb15c294325503818b
Rob Landers <landers.robert@gmail.com>
2019-11-24 20:25:55 +0100

    Add rgb tests

--- csharp/keyboards/ColorSpace/RGB.cs
+++ csharp/keyboards/ColorSpace/RGB.cs
@@ -2,10 +2,22 @@ using System;
 
 namespace keyboards.ColorSpace
 {
+    /// <summary>
+    /// Represents a color in the RGB Space
+    /// </summary>
     public struct Rgb
     {
+        /// <summary>
+        /// An undefined color / black
+        /// </summary>
         public static readonly Rgb Empty = new Rgb();
 
+        /// <summary>
+        /// True if two colors are the same color
+        /// </summary>
+        /// <param name="i1">Left</param>
+        /// <param name="i2">Right</param>
+        /// <returns>True if left and right are the same color</returns>
         public static bool operator ==(Rgb i1, Rgb i2)
         {
             return i1.Red == i2.Red &&
@@ -13,6 +25,12 @@ namespace keyboards.ColorSpace
                    i1.Blue == i2.Blue;
         }
 
+        /// <summary>
+        /// True if two colors are not the same
+        /// </summary>
+        /// <param name="i1">Left</param>
+        /// <param name="i2">Right</param>
+        /// <returns>True if left and right are not the same color</returns>
         public static bool operator !=(Rgb i1, Rgb i2)
         {
             return i1.Red != i2.Red ||
@@ -20,17 +38,49 @@ namespace keyboards.ColorSpace
                    i1.Blue != i2.Blue;
         }
 
+        /// <summary>
+        /// The red component
+        /// </summary>
         public byte Red { get; }
+        
+        /// <summary>
+        /// The green component
+        /// </summary>
         public byte Green { get; }
+        
+        /// <summary>
+        /// The blue component
+        /// </summary>
         public byte Blue { get; }
 
-        public Rgb(byte r, byte g, byte b)
+        /// <summary>
+        /// Create a new color from individual components
+        /// </summary>
+        /// <param name="red">Red</param>
+        /// <param name="green">Green</param>
+        /// <param name="blue">Blue</param>
+        public Rgb(byte red, byte green, byte blue)
         {
-            Red = r;
-            Green = g;
-            Blue = b;
+            Red = red;
+            Green = green;
+            Blue = blue;
         }
 
+        /// <summary>
+        /// Copy an existing color
+        /// </summary>
+        /// <param name="rgb">The color to copy</param>
+        public Rgb(Rgb rgb)
+        {
+            Red = rgb.Red;
+            Green = rgb.Green;
+            Blue = rgb.Blue;
+        }
+
+        /// <summary>
+        /// Convert an HSB color space to RBB color space
+        /// </summary>
+        /// <param name="other">The color to convert</param>
         public Rgb(Hsb other)
         {
             double r = 0, b = 0, g = 0;
@@ -98,11 +148,20 @@ namespace keyboards.ColorSpace
             return Convert.ToByte("0x" + hex, 16);
         }
 
+        /// <summary>
+        /// The string representation of the color
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return "#" + Hex;
         }
 
+        /// <summary>
+        /// Create a new color from a hex representation
+        /// </summary>
+        /// <param name="hex">The hex string</param>
+        /// <returns></returns>
         public static Rgb FromHex(string hex)
         {
             var red = HexToByte(hex.Substring(0,2));
@@ -112,14 +171,26 @@ namespace keyboards.ColorSpace
             return new Rgb(red, gre, blu);
         }
 
+        /// <summary>
+        /// The hex representation of this color
+        /// </summary>
         public string Hex => Red.ToString("X2") + Green.ToString("X2") + Blue.ToString("X2");
 
+        /// <summary>
+        /// True if two colors are the same
+        /// </summary>
+        /// <param name="obj">The object to compare to</param>
+        /// <returns></returns>
         public override bool Equals(object? obj)
         {
             if (obj == null || GetType() != obj.GetType()) return false;
             return this == (Rgb) obj;
         }
 
+        /// <summary>
+        /// Get the hash code of this color
+        /// </summary>
+        /// <returns></returns>
         public override int GetHashCode()
         {
             return Red.GetHashCode() ^ Green.GetHashCode() ^ Blue.GetHashCode();
